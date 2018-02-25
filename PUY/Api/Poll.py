2f7b76d2f71afb67057cfe0034512812e55dff45
Redsamuri <34795680+Redsamuri@users.noreply.github.com>
2018-02-25 20:59:28 +0700

    Update Poll.py

--- PUY/Api/Poll.py
+++ PUY/Api/Poll.py
@@ -18,8 +18,8 @@ class Poll:
   host = "gd2.line.naver.jp";
   port = 443;
 
-  UA = "Line/7.4.7 iPad3,6 7.0.2"
-  LA = "IOSIPAD\t7.4.7\tiPhone OS\t7.0.2"
+  UA = "Line/7.18.0 IOS\t7.18.0\tiPhone OS\t10.12.1"
+  LA = "IOSIPAD\t7.18.0\tiPhone OS\t10.12.1"
 
   rev = 0
 
